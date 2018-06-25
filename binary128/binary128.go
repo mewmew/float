f413d0efef6a3cefd68da2c19cc001fb8eb6a998
mewmew <rnd0x00@gmail.com>
2018-06-25 22:20:33 +0000

    float16,float128: add stub functions

--- binary128/binary128.go
+++ binary128/binary128.go
@@ -1,3 +1,49 @@
-// Package binary128 provides access to the floating-point IEEE 754 quadruple
-// precision format.
+// Package binary128 implements encoding and decoding of IEEE 754 quadruple
+// precision floating-point numbers.
+//
+// https://en.wikipedia.org/wiki/Quadruple-precision_floating-point_format
 package binary128
+
+// Float is a floating-point number in IEEE 754 quadruple precision format.
+type Float struct {
+	// Sign, exponent and fraction.
+	//
+	//    1 bit:    sign
+	//    15 bits:  exponent
+	//    112 bits: fraction
+	a uint64
+	b uint64
+}
+
+// NewFromBits returns the floating-point number corresponding to the IEEE 754
+// quadruple precision binary representation.
+func NewFromBits(a, b uint64) Float {
+	return Float{a: a, b: b}
+}
+
+// NewFromFloat32 returns the nearest quadruple precision floating-point number
+// for x and a bool indicating whether f represents x exactly.
+func NewFromFloat32(x float32) (f Float, exact bool) {
+	panic("not yet implemented")
+}
+
+// NewFromFloat64 returns the nearest quadruple precision floating-point number
+// for x and a bool indicating whether f represents x exactly.
+func NewFromFloat64(x float64) (f Float, exact bool) {
+	panic("not yet implemented")
+}
+
+// Bits returns the IEEE 754 quadruple precision binary representation of f.
+func (f Float) Bits() (a, b uint64) {
+	return f.a, f.b
+}
+
+// Float32 returns the float32 representation of f.
+func (f Float) Float32() float32 {
+	panic("not yet implemented")
+}
+
+// Float64 returns the float64 representation of f.
+func (f Float) Float64() float64 {
+	panic("not yet implemented")
+}
--- binary16/binary16.go
+++ binary16/binary16.go
@@ -1,3 +1,48 @@
-// Package binary16 provides access to the floating-point IEEE 754 half
-// precision format.
+// Package binary16 implements encoding and decoding of IEEE 754 half precision
+// floating-point numbers.
+//
+// https://en.wikipedia.org/wiki/Half-precision_floating-point_format
 package binary16
+
+// Float is a floating-point number in IEEE 754 half precision format.
+type Float struct {
+	// Sign, exponent and fraction.
+	//
+	//    1 bit:   sign
+	//    5 bits:  exponent
+	//    10 bits: fraction
+	a uint16
+}
+
+// NewFromBits returns the floating-point number corresponding to the IEEE 754
+// half precision binary representation.
+func NewFromBits(a uint16) Float {
+	return Float{a: a}
+}
+
+// NewFromFloat32 returns the nearest half precision floating-point number for x
+// and a bool indicating whether f represents x exactly.
+func NewFromFloat32(x float32) (f Float, exact bool) {
+	panic("not yet implemented")
+}
+
+// NewFromFloat64 returns the nearest half precision floating-point number for x
+// and a bool indicating whether f represents x exactly.
+func NewFromFloat64(x float64) (f Float, exact bool) {
+	panic("not yet implemented")
+}
+
+// Bits returns the IEEE 754 half precision binary representation of f.
+func (f Float) Bits() uint16 {
+	return f.a
+}
+
+// Float32 returns the float32 representation of f.
+func (f Float) Float32() float32 {
+	panic("not yet implemented")
+}
+
+// Float64 returns the float64 representation of f.
+func (f Float) Float64() float64 {
+	panic("not yet implemented")
+}
