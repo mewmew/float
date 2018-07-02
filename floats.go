0807f887bd5ab5e425e5181a2848a2d888bcb65a
mewmew <rnd0x00@gmail.com>
2018-07-02 11:37:01 +0000

    floats: add package documentation

--- floats.go
+++ floats.go
@@ -1,3 +1,4 @@
+// Package floats implements multi-precision floating-point numbers.
 package floats
 
 import (
@@ -5,17 +6,21 @@ import (
 	"math/big"
 )
 
+// A Float is a multi-precision floating-point number.
 type Float struct {
 	*big.Float
+	// NaN denotes not-a-number.
 	NaN bool
 }
 
+// New returns a zero value multi-precision floating-point number.
 func New() *Float {
 	return &Float{
 		Float: &big.Float{},
 	}
 }
 
+// Float64 returns the float64 value nearest to f and the associated accuracy.
 func (f *Float) Float64() (float64, big.Accuracy) {
 	if f.NaN {
 		if f.Signbit() {
